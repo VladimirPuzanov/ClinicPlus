a8242657cf6bb49c6e4dabb060d807ce6c5ee074
VladimirPuzanov <volodya938@yandex.ru>
2024-04-30 09:02:00 +0300

    Форматирование кода, незначительные изменения

--- config.php
+++ config.php
@@ -3,4 +3,3 @@ const DB_HOST = "localhost";
 const DB_NAME = "ClinicPlus";
 const DB_USER = "root";
 const DB_PASS = "";
-
--- script.js
+++ script.js
@@ -1,61 +1,58 @@
 const emailInput = $('#userEmail');
 const telInput = $('#userTel');
 
-  function isValidEmail(emailInput) {
-    const pattern = /\w+@\w+/;
-    return pattern.test(emailInput);
-  }
+function isValidEmail(emailInput) {
+  const pattern = /\w+@\w+/;
+  return pattern.test(emailInput);
+}
 
-  function isValidTel(telInput) {
-    const pattern = /^((\+7|7|8)+([0-9]){10})$/;
-    return pattern.test(telInput.replaceAll(" ", ""));
-  }
+function isValidTel(telInput) {
+  const pattern = /^((\+7|7|8)+([0-9]){10})$/;
+  return pattern.test(telInput.replaceAll(" ", ""));
+}
 
 
-$(document).ready(function() {
-    $("#modalFeedbackForm").modal('show');
-    $('#feedbackForm').submit(function(e) {
-        e.preventDefault();
-        const email = emailInput[0].value;
-        const tel = telInput[0].value;
-  
-  if (!isValidEmail(email)) {
-    alert('Пожалуйста введите корректный адрес элетронной почты');
-    return;
-  }
-  
-  if(!isValidTel(tel)) {
-    alert('Введён некорректный телефонный номер');
-    return;
-  }
-        $.ajax({
-            type: "POST",
-            url: 'feedbackForm.php',
-            data: $(this).serialize(),
-            success: function(response)
-            {
-                var jsonData = JSON.parse(response);
-                if (jsonData.success == "1")
-                {
-                    $("#modalFeedbackForm").modal('hide');
-                    alert("Ваша заявка успешно отправлена");
-                }
-                else
-                {
-                    if(jsonData.error == "RF"){
-                        $('#SubmitError').removeClass('d-none');
-                    }
-                    if(jsonData.error == "NVD"){
-                        console.log("Не валидные данные");
-                    }
-                }
-           }
-       });
-     })
+$(document).ready(function () {
+  $("#modalFeedbackForm").modal('show');
+  $('#feedbackForm').submit(function (e) {
+    e.preventDefault();
+    const email = emailInput[0].value;
+    const tel = telInput[0].value;
+
+    if (!isValidEmail(email)) {
+      alert('Пожалуйста введите корректный адрес элетронной почты');
+      return;
+    }
+
+    if (!isValidTel(tel)) {
+      alert('Введён некорректный телефонный номер');
+      return;
+    }
+    $.ajax({
+      type: "POST",
+      url: 'feedbackForm.php',
+      data: $(this).serialize(),
+      success: function (response) {
+        var jsonData = JSON.parse(response);
+        if (jsonData.success == "1") {
+          $("#modalFeedbackForm").modal('hide');
+          alert("Ваша заявка успешно отправлена");
+        }
+        else {
+          if (jsonData.error == "RF") {
+            $("#submitError").removeClass("d-none");
+          }
+          if (jsonData.error == "NVD") {
+            console.log("Не валидные данные");
+          }
+        }
+      }
+    });
+  })
 });
 
 var cleave = new Cleave('#userTel', {
-    phone: true,
-    phoneRegionCode: 'RU'
+  phone: true,
+  phoneRegionCode: 'RU'
 });
 
